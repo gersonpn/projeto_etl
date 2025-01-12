5b526eb61c815488d3515a7c563a75bee9dfaaa0
gersonpn <gerson.neto@estudante.ifms.edu.br>
2025-01-12 00:57:30 -0400

    dag de baixar arquivos ajustada

--- infra/airflow/dags/baixar_csv_dag.py
+++ infra/airflow/dags/baixar_csv_dag.py
@@ -35,9 +35,10 @@ dag = DAG(
     schedule_interval='@monthly',
 )
 
+
 download_task = PythonOperator(
     task_id='baixar_arquivos',
     python_callable=baixar_arquivos_csv,
-    op_args=[2017, '/opt/airflow/data'],
+    op_args=[2017, '/opt/airflow/data/raw'],
     dag=dag,
 )
