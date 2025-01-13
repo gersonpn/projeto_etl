22d8dae01c5437c8561997ae92291372287e0003
gersonpn <gerson.neto@estudante.ifms.edu.br>
2025-01-13 15:27:21 -0400

    ajuste pasta raiz do projeto

--- airflow/dags/baixar_csv_dag.py
+++ airflow/dags/baixar_csv_dag.py
@@ -1,33 +1,6 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
-import os
-import requests
-
-def baixar_arquivos_csv(ano_inicial, pasta):
-    ano_atual = datetime.now().year
-    os.makedirs(pasta, exist_ok=True)
-
-
-
-    for ano in range(ano_inicial, ano_atual + 1):
-
-        pasta_ano = os.path.join(pasta, str(ano))
-        os.makedirs(pasta_ano, exist_ok=True)
-
-        for mes in range(1, 13):
-            meses = f"{mes:02d}"
-            url = f"https://sistemas.anac.gov.br/dadosabertos/Aeronaves/RAB/Historico_RAB/{ano}-{meses}.csv"
-            nome_arquivo = os.path.join(pasta_ano, f"{ano}-{meses}.csv")
-
-            try:
-                response = requests.get(url)
-                response.raise_for_status()
-                with open(nome_arquivo, 'wb') as f:
-                    f.write(response.content)
-                print(f"Arquivo {nome_arquivo} salvo com sucesso!")
-            except requests.exceptions.RequestException as e:
-                print(f"Erro ao baixar o arquivo {url}: {e}")
+from task.task import create_download_task
 
 default_args = {
     'owner': 'airflow',
@@ -35,16 +8,11 @@ default_args = {
     'start_date': datetime(2017, 1, 1),
 }
 
-dag = DAG(
+with DAG(
     'baixar_arquivos_csv',
     default_args=default_args,
     schedule_interval='@monthly',
-)
+    catchup=False,
+) as dag:
 
-
-download_task = PythonOperator(
-    task_id='baixar_arquivos',
-    python_callable=baixar_arquivos_csv,
-    op_args=[2017, '/opt/airflow/data/raw'],
-    dag=dag,
-)
+    download_task = create_download_task(dag)
