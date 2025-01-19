db107ddbcea71cda04231f8040f7e3068d7b4fab
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2025-01-19 12:10:20 -0400

    fix: ajuste na variavel de ambiente e adicionando log na task

--- airflow/dags/anac_rab_data_ingestion/tasks/ingest.py
+++ airflow/dags/anac_rab_data_ingestion/tasks/ingest.py
@@ -1,6 +1,6 @@
 import os
 import requests
-
+import logging
 def baixar_arquivos_csv(ano_inicial, mes, pasta):
     """
     Baixa arquivos CSV de acordo com o ano e mês especificados.
@@ -8,7 +8,7 @@ def baixar_arquivos_csv(ano_inicial, mes, pasta):
     os.makedirs(pasta, exist_ok=True)
     url = f"https://sistemas.anac.gov.br/dadosabertos/Aeronaves/RAB/Historico_RAB/{ano_inicial}-{mes:02d}.csv"
     nome_arquivo = os.path.join(pasta, f"{ano_inicial}-{mes:02d}.csv")
-
+    logging.info(f"Baixando arquivo {nome_arquivo}...")
     try:
         response = requests.head(url, timeout=10)
         if response.status_code == 200:
@@ -16,7 +16,9 @@ def baixar_arquivos_csv(ano_inicial, mes, pasta):
             response.raise_for_status()
             with open(nome_arquivo, 'wb') as f:
                 f.write(response.content)
+            logging.info(f"Arquivo {nome_arquivo} salvo com sucesso!")
         else:
             raise ValueError(f"Arquivo não encontrado: {url}")
     except Exception as e:
+        logging.error(f"Erro ao baixar {url}: {str(e)}")
         raise RuntimeError(f"Erro ao baixar {url}: {str(e)}")
