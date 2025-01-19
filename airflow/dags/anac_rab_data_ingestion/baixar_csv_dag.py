5dbc3c69f324ba976547436a54baa2def818b6e6
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2025-01-19 11:51:32 -0400

    fix: adição de task group e adicionando a ingestão em paralelo

--- airflow/dags/anac_rab_data_ingestion/baixar_csv_dag.py
+++ airflow/dags/anac_rab_data_ingestion/baixar_csv_dag.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.task_group import TaskGroup
 from anac_rab_data_ingestion.tasks.ingest import baixar_arquivos_csv
 
 default_args = {
@@ -20,25 +22,36 @@ with DAG(
     current_year = datetime.now().year
     current_month = datetime.now().month
 
-    tasks = []
+    # Faltou criar as Dummies
 
-    for ano in range(2017, current_year + 1):
-        for mes in range(1, 13):
-            # Evita criar tasks para meses futuros no ano atual
-            if ano == current_year and mes > current_month:
-                break
+    begin_ingestion = DummyOperator(task_id='begin_ingestion')
+    end_ingestion = DummyOperator(task_id='end_ingestion')
+    begin_transform = DummyOperator(task_id='begin_transform')
+    end_transform = DummyOperator(task_id='end_transform')
+    begin_load = DummyOperator(task_id='begin_load')
+    end_load = DummyOperator(task_id='end_load')
+  
+    # Você também poderia criar um grupo de tasks para baixar os arquivos
 
-            task = PythonOperator(
-                task_id=f'baixar_{ano}_{mes:02d}',
-                python_callable=baixar_arquivos_csv,
-                op_kwargs={
-                    'ano_inicial': ano,
-                    'mes': mes,
-                    'pasta': '/opt/data/raw',
-                },
-            )
-            tasks.append(task)
+    with TaskGroup('rab_data_ingestion') as rab_data_ingestion:
 
-    # Define dependências (opcional, caso precise rodar sequencialmente)
-    for i in range(1, len(tasks)):
-        tasks[i - 1] >> tasks[i]
+        for ano in range(2017, current_year + 1):
+            for mes in range(1, 13):
+                # Evita criar tasks para meses futuros no ano atual
+                if ano == current_year and mes > current_month:
+                    break
+
+                task = PythonOperator(
+                    task_id=f'baixar_{ano}_{mes:02d}',
+                    python_callable=baixar_arquivos_csv,
+                    op_kwargs={
+                        'ano_inicial': ano,
+                        'mes': mes,
+                        'pasta': '/opt/data/raw',
+                    },
+                )
+                begin_ingestion >> task >> end_ingestion
+        
+    # TODO: Criar as tasks de transformação e carga
+    
+    end_ingestion >> begin_transform >> end_transform >> begin_load >> end_load
